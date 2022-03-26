cec90019419a285c6c73211df73d2ab66f552727
Peter Kosarnik <nestastnikos@github.com>
2022-03-26 09:45:42 +0100

    feat: add representation of virtual spaces

--- src/Fei.Backend.Main/Program.fs
+++ src/Fei.Backend.Main/Program.fs
@@ -1,8 +1,46 @@
 ﻿open System.IO
+open System
 open Audit
 
+type IsRecursive = bool
+
+
+[<FlagsAttribute>]
+type VirtualSpacePermissions = Read = 1 | Write = 2 | See = 4
+
+let AllVsPermissions = VirtualSpacePermissions.Read ||| VirtualSpacePermissions.Write ||| VirtualSpacePermissions.See
+
+
+type PathEntry = {
+  PathName: string;
+  IsRecursive: bool;
+  IsAddition: bool;
+}
+
+
+type VirtualSpaceFilesystem = {
+  Name: string;
+  Paths: PathEntry list
+}
+
+
+type VirtualSpaceProcess = {
+  Name: string;
+  EnteredTree: string * IsRecursive;
+  VirtualSpaces: (VirtualSpaceFilesystem * VirtualSpacePermissions) list;
+}
+
+let allFilesEntry = { PathName = "/"; IsRecursive = true; IsAddition = true; }
+let allFilesVs = { Name = "all_files"; Paths = List.singleton allFilesEntry}
+let allProcessesVs = {
+  Name = "all_processes";
+  EnteredTree = ("domain",true);
+  VirtualSpaces = List.singleton (allFilesVs, AllVsPermissions)
+  }
 
 "./Fei.Backend.Main/Resources/example-mysqld_t.log"
   |> File.ReadAllLines
   |> parseToAuditLogEntries
   |> Seq.iter (printfn "%A")
+
+printfn "%A" allProcessesVs
