ab84b34e5fddc626f01af3c889a58d78e0f2652b
Peter Kosarnik <nestastnikos@github.com>
2022-05-22 20:26:41 +0200

    feat: add cmd arguments to constable config

--- src/Fei.ConstableConfig.Main/Program.fs
+++ src/Fei.ConstableConfig.Main/Program.fs
@@ -1,11 +1,23 @@
 ﻿open System.IO
 open Newtonsoft.Json
+open System
 
+[<EntryPoint>]
+let main (args) =
+  let (input, output) =
+    match args with
+    | [||] ->
+      ("policy.json", "constable.conf")
+    | [|first; second|] ->
+      (first, second)
+    | _ ->
+      raise (ArgumentException("Invalid number of command line arguments"))
 
-let rules =
-  JsonConvert.DeserializeObject<InputMapping.InputRuleDto list>("./test.json" |> File.ReadAllText)
-  |> List.map (fun x -> InputMapping.ruleDtoToRule x)
+  let rules =
+    JsonConvert.DeserializeObject<InputMapping.InputRuleDto list>(input |> File.ReadAllText)
+    |> List.map (fun x -> InputMapping.ruleDtoToRule x)
 
-let context = GrammarContext.createDefault rules
-let result = GrammarContext.createLanguage context
-("constable.conf", result) |> File.WriteAllText
+  let context = GrammarContext.createDefault rules
+  let result = GrammarContext.createLanguage context
+  (output, result) |> File.WriteAllText
+  0
